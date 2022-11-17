6849c5c80836310098b82c490f88004d577260e2
Raghu K <raghuk.vit@gmail.com>
2022-11-17 08:47:39 +0530

    Frontend Script

---
+++ frontend.sh
@@ -0,0 +1,12 @@
+yum install nginx -y
+
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/static/* .
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl enable nginx
+systemctl restart nginx
