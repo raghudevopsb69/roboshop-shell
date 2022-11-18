0233865e5629a8ecfd0658a792af8230336de3f5
Raghu K <raghuk.vit@gmail.com>
2022-11-18 08:13:52 +0530

    Redis Script

---
+++ redis.sh
@@ -0,0 +1,9 @@
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+dnf module enable redis:remi-6.2 -y
+yum install redis -y
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+
+systemctl enable redis
+systemctl restart redis
+
