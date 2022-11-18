8ded46f44c416ef8f47c0ba2b07c2cafd5b474e5
Raghu K <raghuk.vit@gmail.com>
2022-11-18 07:59:37 +0530

    Mongo Script

--- mongo.sh
+++ mongo.sh
@@ -5,3 +5,10 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod
 systemctl restart mongod
+
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+cd /tmp
+unzip mongodb.zip
+cd mongodb-main
+mongo < catalogue.js
+mongo < users.js
