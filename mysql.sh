533d141981d4885d566f79a8d2381551fd505ffa
Raghu K <raghuk.vit@gmail.com>
2022-11-18 08:27:17 +0530

    MySQL Script

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-dnf module disable mysql
+dnf module disable mysql -y
 
 yum install mysql-community-server -y
 
