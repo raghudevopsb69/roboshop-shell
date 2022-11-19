7785701aa8e3541a0e4252bc178c62b5527e268f
Raghu K <raghuk.vit@gmail.com>
2022-11-19 08:29:46 +0530

    MySQL Script

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,40 @@
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 dnf module disable mysql -y
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 
 yum install mysql-community-server -y
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 
 systemctl enable mysqld
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 systemctl restart mysqld
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
+
 
 echo show databases | mysql -uroot -pRoboShop@1
 if [ $? -ne 0 ]
