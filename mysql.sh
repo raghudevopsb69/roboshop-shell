dad61cd0e73dacef7a61e860a379b7b688c3a050
Raghu K <raghuk.vit@gmail.com>
2022-11-21 08:37:21 +0530

    Update roboshop-shell/

--- mysql.sh
+++ mysql.sh
@@ -5,50 +5,34 @@ fi
 
 ROBOSHOP_MYSQL_PASSWORD=$1
 
+STAT() {
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+    exit
+  fi
+}
+
 echo -e "\e[33mDownloading MySQL Repo File\e[0m"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $?
 
 echo Disable MySQL 8 Version repo
 dnf module disable mysql -y
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $?
 
 echo Install MySQL
 yum install mysql-community-server -y
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $?
 
 echo Enable MySQL Service
 systemctl enable mysqld
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $?
 
 echo Start MySQL Service
 systemctl restart mysqld
-if [ $? -eq 0 ]; then
-  echo SUCCESS
-else
-  echo FAILURE
-  exit
-fi
+STAT $? 
 
 
 echo show databases | mysql -uroot -p${ROBOSHOP_MYSQL_PASSWORD}
