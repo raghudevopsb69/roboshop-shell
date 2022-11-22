c637afcb98e7a0a801a7c673117e5be6d2904776
Raghu K <raghuk.vit@gmail.com>
2022-11-22 08:00:32 +0530

    Update roboshop-shell/

--- common.sh
+++ common.sh
@@ -3,12 +3,13 @@ STAT() {
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
-    echo Check the error in $LOG file 
+    echo Check the error in $LOG file
     exit
   fi
 }
 
 PRINT() {
+  echo " --------------------  $LOG ------------------" >>${LOG}
   echo -e "\e[33m$1\e[0m"
 }
 
