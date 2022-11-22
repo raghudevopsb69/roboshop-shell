834d78b13afc17cc3ab58531049b62350d3a6e4f
Raghu K <raghuk.vit@gmail.com>
2022-11-22 08:33:22 +0530

    Update roboshop-shell/

--- redis.sh
+++ redis.sh
@@ -1,9 +1,27 @@
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
-yum install redis -y
+COMPONENT=redis
+source common.sh
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+PRINT "Install Redis Repo"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOG
+STAT $?
 
-systemctl enable redis
-systemctl restart redis
+PRINT "Enable Redis Repo 6.2"
+dnf module enable redis:remi-6.2 -y &>>$LOG
+STAT $?
+
+PRINT "Install Redis"
+yum install redis -y &>>$LOG
+STAT $?
+
+PRINT "Update Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>$LOG
+STAT $?
+
+PRINT "Enable Redis Service"
+systemctl enable redis &>>$LOG
+STAT $?
+
+PRINT "Start Redis Service"
+systemctl restart redis &>>$LOG
+STAT $?
 
