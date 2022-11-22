0d9fa400c5d55e5c4f8382ef9ce996573c11e8d4
Raghu K <raghuk.vit@gmail.com>
2022-11-22 08:24:56 +0530

    Update roboshop-shell/

--- frontend.sh
+++ frontend.sh
@@ -9,10 +9,19 @@ STAT $?
 APP_LOC=/usr/share/nginx/html
 
 DOWNLOAD_APP_CODE
-exit
 
 mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
-systemctl enable nginx
-systemctl restart nginx
+PRINT "Copy RoboShop Configuration File"
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+
+PRINT "Enable Nginx Service"
+systemctl enable nginx &>>$LOG
+STAT $?
+
+PRINT "Start Nginx Service"
+systemctl restart nginx &>>$LOG
+STAT $?
+
