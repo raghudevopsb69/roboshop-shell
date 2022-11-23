c401a9044248f74243736dfc1e948b8acced13b6
Raghu K <raghuk.vit@gmail.com>
2022-11-23 08:14:16 +0530

    Update roboshop-shell/

--- create-roboshop-servers.sh
+++ create-roboshop-servers.sh
@@ -19,7 +19,6 @@ create_ec2() {
 
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
-  exit
 }
 
 
