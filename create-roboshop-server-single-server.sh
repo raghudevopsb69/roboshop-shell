f38c65956b878a4223b39d9980c093a3f45d975b
Raghu K <raghuk.vit@gmail.com>
2022-11-30 07:42:56 +0530

    Update roboshop-shell/

---
+++ create-roboshop-server-single-server.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+##### Change these values ###
+ZONE_ID="Z02997672I591AG282OU5"
+SG_NAME="allow-all"
+#ENV="dev"
+#############################
+
+env=dev
+
+create_ec2() {
+  PRIVATE_IP=$(aws ec2 run-instances \
+      --image-id ${AMI_ID} \
+      --instance-type t3.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --security-group-ids ${SGID} \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
+
+
+## Main Program
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-8-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+if [ -z "${AMI_ID}" ]; then
+  echo "AMI_ID not found"
+  exit 1
+fi
+
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+if [ -z "${SGID}" ]; then
+  echo "Given Security Group does not exit"
+  exit 1
+fi
+
+if [ -z "$1" ]; then
+  echo Input Component Name is needed
+  exit 1
+fi
+
+COMPONENT="${env}-${component}"
+create_ec2
+
