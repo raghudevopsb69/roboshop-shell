c82e46dc4cd7803064792b92e5c59fb53613dde1
Raghu K <raghuk.vit@gmail.com>
2022-12-08 07:52:04 +0530

    Update roboshop-shell/

--- create-roboshop-server-single-server.sh
+++ create-roboshop-server-single-server.sh
@@ -15,7 +15,7 @@ create_ec2() {
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
-      --iam-instance-profile aws_ssm_dev_role \
+      --iam-instance-profile Name=aws_ssm_dev_role \
       | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
   sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
