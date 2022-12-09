d7993fa836726bc13555ccface3fdcba0a23ce4d
Raghu K <raghuk.vit@gmail.com>
2022-12-09 08:15:15 +0530

    Update roboshop-shell/

--- create-roboshop-server-single-server.sh
+++ create-roboshop-server-single-server.sh
@@ -12,7 +12,7 @@ create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
       --instance-type t3.micro \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
       --iam-instance-profile Name=aws_ssm_dev_role \
